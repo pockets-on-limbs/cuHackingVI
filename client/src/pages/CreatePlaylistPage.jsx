8df5ac614ac7f0749046f38f15bcdb16fbbbd5f5
Isha Viragi <isha.viragi@gmail.com>
2025-03-15 19:16:28 -0400

    Set up for fixing routes

---
+++ client/src/pages/CreatePlaylistPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./MainPage.css";
+import { NextPage } from "./NextPage.jsx";
+import { useState } from "react";
+
+export function CreatePlaylistPage() {
+  const [showNextPage, setShowNextPage] = useState(false);
+
+  return (
+    <div className="mainpage">
+      {showNextPage ? (
+        <NextPage />
+      ) : (
+        <>
+          <h2>Spotify Clone</h2>
+          <button onClick={() => setShowNextPage(true)}>Next Page</button>
+        </>
+      )}
+    </div>
+  );
+}
