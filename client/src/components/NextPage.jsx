e3b709e121ceab72b193f5981e4bc4bc2875bd18
Isha Viragi <isha.viragi@gmail.com>
2025-03-15 19:01:09 -0400

    Figure out how to use without routes

---
+++ client/src/components/NextPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NextPage.css";
+
+export function NextPage() {
+  return (
+    <div className="nextpage">
+      <h2>Page 2</h2>
+      Next page
+    </div>
+  );
+}
