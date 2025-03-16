442accc1f8b92cb8bb130fa3fde548a164b5641e
unknown <mailcatherines@gmail.com>
2025-03-16 07:15:13 -0400

    ui

--- server/main.py
+++ server/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from api import song, gemini
 
-import essentia
-
 app = FastAPI()
 
 app.add_middleware(
