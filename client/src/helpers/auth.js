d0b262b0788a8facb3405b6ee64d26dd993e4887
catherine-sun <59343226+catherine-sun@users.noreply.github.com>
2025-03-16 11:50:38 -0400

    Update auth.js

--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -26,13 +26,13 @@ export const authenticate = async () => {
   const authUrl = new URL("https://accounts.spotify.com/authorize");
   window.localStorage.setItem("code_verifier", codeVerifier);
 
-  console.log(process.env.VITE_SPOTIFY_REDIRECT_URI);
+  console.log(import.meta.env.VITE_SPOTIFY_REDIRECT_URI);
   const params = {
     response_type: "code",
-    client_id: process.env.VITE_SPOTIFY_CLIENT_ID,
+    client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
     code_challenge_method: "S256",
     code_challenge: codeChallenge,
-    redirect_uri: process.env.VITE_SPOTIFY_REDIRECT_URI,
+    redirect_uri: import.meta.env.VITE_SPOTIFY_REDIRECT_URI,
     scope: "playlist-read-private",
   };
 
@@ -50,10 +50,10 @@ export const getToken = async (code) => {
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
-      client_id: process.env.VITE_SPOTIFY_CLIENT_ID,
+      client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
       grant_type: "authorization_code",
       code,
-      redirect_uri: process.env.VITE_SPOTIFY_REDIRECT_URI,
+      redirect_uri: import.meta.env.VITE_SPOTIFY_REDIRECT_URI,
       code_verifier: codeVerifier,
     }),
   };
