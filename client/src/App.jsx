d7c79e0e34af2d15098b6917dc1cbc2aa23a64e1
Isha Viragi <156847717+Isha-Viragi@users.noreply.github.com>
2025-03-15 21:03:17 -0400

    Client (#4)
    * Initial base ui
    
    * Set up components, pages and routes
    
    * Add styles to LoginPage
    
    * Add core functionality to RateSongsPage
    
    * Set up for fixing routes
    
    * Rewrite code
    
    * Fix routes
    
    * Reaply basic styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,34 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Auth, getToken } from './authentication/Auth.jsx'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Auth, getToken } from "./authentication/Auth.jsx";
+import { CreatePlaylistPage } from "./pages/CreatePlaylistPage.jsx";
 
 function App() {
-  const [login, setLogin] = useState(null)
-  const [access, setAccess] = useState(sessionStorage.getItem('access_token'))
-
+  const [login, setLogin] = useState(null);
+  const [access, setAccess] = useState(sessionStorage.getItem("access_token"));
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
 
-    if (urlParams.has('code')) {
-      let code = urlParams.get('code')
-      sessionStorage.setItem('code', code);
+    if (urlParams.has("code")) {
+      let code = urlParams.get("code");
+      sessionStorage.setItem("code", code);
 
-      getToken(code).then(token => {
-        if(token) {
-          sessionStorage.setItem('access_token', token)
+      getToken(code).then((token) => {
+        if (token) {
+          sessionStorage.setItem("access_token", token);
           setAccess(true);
         }
-      })
+      });
 
-      urlParams.delete('code');
-      window.history.replaceState('null', '', window.location.pathname)
+      urlParams.delete("code");
+      window.history.replaceState("null", "", window.location.pathname);
     } else {
       setLogin(Auth());
     }
-  }, [])
+  }, []);
 
-  return (
-    access ? <p style={{"color": "white"}}>YOU ARE IN THE APP</p>
-    : login
-  )
+  return access ? <CreatePlaylistPage /> : login;
 }
 
-export default App
+export default App;
