1636c5677be016d446f47e3567e8cde647dcdefa
Mashhood Khan <mashhoodkhan71@gmail.com>
2025-03-17 23:39:12 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { authenticate, getToken } from "./helpers/auth";
+import { generateCodeChallengeAndVerifier, authenticate, getToken } from "./helpers/auth";
 import Home from "./pages/Home";
 
 function App() {
